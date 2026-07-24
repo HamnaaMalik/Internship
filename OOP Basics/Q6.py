23d3a8df32cb56f3134558f6e1f26a8a0e0dc143
HamnaaMalik <hamna.malik.stu@gmail.com>
2026-07-24 19:32:27 +0500

    Update and rename Q6.py to OOP Basics/Q6.py

---
+++ OOP Basics/Q6.py
@@ -0,0 +1,9 @@
+#Question 06: Automated Vehicle Diagnostic (Simple Method)
+#Ek Vehicle class banayein jismein ek method run_diagnostic() ho jo print kare "System check complete: 0 errors found.
+
+class Vehicle:
+    def run_diagnostic(self):
+        print("System check complete: 0 errors found.")
+
+car = Vehicle()
+car.run_diagnostic()
